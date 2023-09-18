f604b97870c340160e157028fa9f99f3118d2d75
Virginie Dourson <virginiedourson@gmail.com>
2023-09-18 11:42:08 +0200

    Introduction

---
+++ 1.Introduction/script.js
@@ -0,0 +1,16 @@
+/*
+*/
+
+//This function takes as argument an integer "size",
+// and generate a random integer between 0 and "size".
+function randomN(size){
+    return Math.floor(Math.random()*size);
+}
+
+document.querySelector("h1").firstChild.textContent= "Modifying the DOM | ";
+document.body.style.backgroundColor = `rgb(${randomN(256)}, ${randomN(256)}, ${randomN(256)})`;
+
+for (let child of document.body.children){
+    console.log(child);
+} 
+
