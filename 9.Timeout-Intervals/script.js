6d381383f56028cb4277bef9e8565ea872238ee7
Virginie Dourson <virginiedourson@gmail.com>
2023-09-29 11:30:36 +0200

    Ex2

--- 9.Timeout-Intervals/script.js
+++ 9.Timeout-Intervals/script.js
@@ -10,3 +10,25 @@ let typewriter = setInterval(() =>
     i < word.length ? place.textContent += word[i++] : clearInterval(typewriter)
 ,1000);
 
+//Write a function that displays every second that has passed on the page since it was opened.
+//The display should be refreshed every second.
+//If 60 seconds are elapsed, write "a minute has passed", then "2 minutes have passed", etc.
+
+const _initTime = Date.now()
+
+let displaySeconds = document.createElement('div');
+document.body.appendChild(displaySeconds);
+let seconds = 0;
+
+let displayMinutes = document.createElement('p');
+document.body.appendChild(displayMinutes);
+let minutes = 0;
+
+const writeElapsedTime = () => {
+    displaySeconds.textContent = ++seconds + ' s';
+    if (Math.floor(seconds/60) > minutes) {
+        displayMinutes.textContent = ++minutes + ' minutes has elapsed'
+    }
+}
+
+setInterval(writeElapsedTime, 1000);
