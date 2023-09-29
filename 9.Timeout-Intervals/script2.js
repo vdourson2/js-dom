28e870a1be5728076a72ac73e3c9e52da35fa2c0
Virginie Dourson <virginiedourson@gmail.com>
2023-09-29 15:47:19 +0200

    Improve and refactor

--- 9.Timeout-Intervals/script2.js
+++ 9.Timeout-Intervals/script2.js
@@ -4,21 +4,40 @@ function randomN(size){
     return Math.floor(Math.random()*size);
 }
 let circles = document.querySelectorAll('.game__circle');
+
+
 let scoreNode = document.querySelector('.wrapScore__score');
 let score = 0;
-circles.forEach((circle) =>
+//Add event on each div to increase score if the click happen when the div is red
+for (let circle of circles) {
     circle.addEventListener('click', (e) => {
         if (e.target.style.backgroundColor == "red") {
             scoreNode.textContent = ++score;
         }
-    }))
-
+    })
+}
+let previousPlaceNb = 0;
+let counter = 0;
 let play = setInterval(() => {
-    circles.forEach((circle) => circle.style.backgroundColor = "white");
-    let placeNb = randomN(9);
-    let place = circles[placeNb];
-    place.style.backgroundColor = "red";
-}, 1000);
+
+    //Color the previous red div in white to reset the screen:
+    circles[previousPlaceNb].style.backgroundColor = "white";
+    if (counter++ == 20) { //Increase counter and end game after 50 times of the mole appearance :
+        clearInterval(play);
+    }
+    else {
+        //Generates a random number different from the precedent one,
+        //and color the corresponding div in red : 
+        let placeNb = randomN(9);
+        while (placeNb == previousPlaceNb) {
+            placeNb = randomN(9);
+        }
+        previousPlaceNb = placeNb;
+        circles[placeNb].style.backgroundColor = "red";
+    }
+
+    
+}, 800);
 
 
 
