cdc29ca4e8202e8c925d9d35f940589018c31871
Virginie Dourson <virginiedourson@gmail.com>
2023-10-02 17:01:43 +0200

    Exercise 4 toggle hour12

--- 10.Dates/Exercice4/script.js
+++ 10.Dates/Exercice4/script.js
@@ -1,3 +1,8 @@
+let display12 = 0;
+document.querySelector(".hour__hours").addEventListener('click',(e) => {
+    display12 = (display12 == 0) ? 1 : 0;
+})
+
 function display() {
     let now = new Date();
     document.querySelector(".wrapCalendar__weekday").textContent = now.toLocaleDateString('en-GB',{weekday:"short"});
@@ -5,7 +10,12 @@ function display() {
     document.querySelector(".wrapCalendar__month").textContent = now.toLocaleDateString('en-GB',{month:"short"});
     document.querySelector(".wrapCalendar__year").textContent = now.toLocaleDateString('en-GB',{year:"numeric"});
     document.querySelector(".wrapCalendar__weekday").textContent = now.toLocaleDateString('en-GB',{weekday:"short"});
-    document.querySelector(".hour__minutes").textContent = now.toLocaleTimeString('en-GB',{hour:'numeric',minute:"numeric"}) + ": ";
+    document.querySelector(".hour__minutes").textContent = now.toLocaleTimeString('en-GB',{minute:"numeric"}) + ": ";
     document.querySelector(".hour__seconds").textContent = now.toLocaleTimeString('en-GB',{second:'2-digit'});
+    if (display12 == 1) {
+        document.querySelector(".hour__hours").textContent = (now.toLocaleTimeString('en-GB',{hour:'numeric',hour12:'true'})).split(" ")[0] + ": ";
+    } else {
+        document.querySelector(".hour__hours").textContent = now.toLocaleTimeString('en-GB',{hour:'numeric'}) + ": ";
+    }    
 }
 setInterval(() => display(), 1000);
