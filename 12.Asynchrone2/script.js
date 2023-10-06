bf1fa4850c970a7aeade83a18ccfde6576c1e282
Virginie Dourson <virginiedourson@gmail.com>
2023-10-06 16:02:35 +0200

    Asynchrone ex2 WIP2

--- 12.Asynchrone2/script.js
+++ 12.Asynchrone2/script.js
@@ -3,20 +3,26 @@ btn.addEventListener('click', () => {
     let input = document.querySelector('input');
     let select = document.querySelector('select');
     let div = document.createElement('div');
-    if (localStorage.getItem(input.value) == null) {
+    document.body.appendChild(div);
+    let resInLocalStorage = JSON.parse(localStorage.getItem(input.value))
+
+    //If the name with the same country is already in the local storage,
+    //then take the data from there
+    if ((resInLocalStorage !== null) && (resInLocalStorage.country_id == select.value)) {
+        div.textContent = `${res.name}, country : ${select.value}, age : ${res.age}`
+        input.value = '';
+    } 
+    //If the name isn't already in the local storage or if the country is different,
+    //then fech data from the API, and store them in local storage for further use
+    else {
         fetch("https://api.agify.io/?name=" + input.value + "&country_id=" + select.value)
             .then((res) => res.json())
             .then((res) => {
-                // localStorage.clear();
                 localStorage.setItem(`${input.value}`, JSON.stringify(res));
-                // let div = document.createElement('div');
                 div.textContent = `${res.name}, country : ${select.value}, age : ${res.age}`
-                document.body.appendChild(div);
                 input.value = '';
             })
             .catch((e) => console.log(e))
-    } 
-    else {
-        console.log('WIP');
     }
+    //localStorage.clear();
 })
